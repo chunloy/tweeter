9be243dee64e9cdf9c7f05ac1bc1830952b235d5
Aaron Au <aaron.au@me.com>
2022-04-27 22:23:48 -0600

    Implement refetch functionality on form submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -32,16 +32,30 @@ const createTweetElement = function(userData) {
 
 //render tweets in index.html
 const renderTweets = function(data) {
+
+  //empty container to prevent duplicates on reder
+  $('#tweet-container').empty();
+
   for (const user of data) {
     $('#tweets-container').prepend(createTweetElement(user));
   }
 };
 
 $(document).ready(function() {
+
+  const loadTweets = function() {
+    $.get('/tweets', function(data) {
+      renderTweets(data);
+    });
+  };
+
   //event triggers when button is pressed
   $('form').submit(function(event) {
+
     event.preventDefault();
     const textInput = $('#tweet-text').val().trim();
+
+    //render html using .json file
 
     //send alert if input text is empty
     if (!textInput) {
@@ -53,17 +67,15 @@ $(document).ready(function() {
       return alert(`Content exceeds the ${MAX_CHARS} character limit. Please consider shortening the tweet.`);
     }
 
-    console.log('here');
     //send input data to .json file
-    $.post('http://localhost:8080/tweets/', $('#tweet-text').serialize(), function() {
+    $.post('http://localhost:8080/tweets', $('#tweet-text').serialize(), function() {
       loadTweets();
-    });
 
-    //render html using .json file
-    const loadTweets = function() {
-      $.get('http://localhost:8080/tweets/', function(data) {
-        renderTweets(data);
-      });
-    };
+      //clear form after posting
+      $('form').trigger('reset');
+    });
   });
+
+  //load database when ready
+  loadTweets();
 });
